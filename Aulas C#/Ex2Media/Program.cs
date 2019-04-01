ce3e4c513771cdfe1408d19f5c34b55b0358e2dd
Bruno Henrique Silva <38940293827@info.local>
2019-04-01 08:23:00 -0300

    Aulas C#

---
+++ Aulas C#/Ex2Media/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Aulas_C_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Calculando a media");
+            float nota1=9;
+            float nota2=10;
+            float nota3=5;
+
+            float soma;
+            
+            soma = nota1 + nota2 + nota3;
+            float media = soma/3;
+            Console.WriteLine("A soma das notas é: "+soma+" e a média é: "+media);
+        }
+    }
+}
