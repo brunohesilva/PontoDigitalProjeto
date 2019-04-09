0e55fd9258181c89e8570eaa1d0a1dd72b782936
Bruno Henrique Silva <38940293827@info.local>
2019-04-09 11:24:30 -0300

    U

---
+++ Aulas C#/ArrayAirLines/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace ArrayAirLines
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // DateTime data = DateTime.Parse(Console.ReadLine());
+
+            int respostas;
+            int[] pessoa = new int[5];
+            int[] numPassage = new int[5];
+            int[] data = new int[5];
+
+            Console.WriteLine("Bem vindo á OnurAirLines");
+            Console.WriteLine("------------------------");
+            Console.WriteLine("1- Registrar Passagem");
+            Console.WriteLine("2- Visualizar Passagens");
+            Console.WriteLine("0- Sair");
+            Console.WriteLine("------------------------");
+
+            switch (respostas) {
+                case "1":
+                Console.WriteLine("Nome do Passageiro");
+                int pessoa = int.Parse(Console.ReadLine());
+          
+                default:
+            }
+
+            // int resposta = int.Parse(Console.ReadLine());
+            // Console.WriteLine("OBRIGAO POR NADA!!!");
+
+        }
+    }
+}
--- Aulas C#/ArrayCaixa/Program.cs
+++ Aulas C#/ArrayCaixa/Program.cs
@@ -6,45 +6,64 @@ namespace ArrayCaixa
     {
         static void Main(string[] args)
         {
-            // int[] notas = {2,5,10,20,50,100};
+            // int[] notas = {1,2,5,10,20,50,100};
 
-            Console.WriteLine("Caixa Eletronico");
-            Console.WriteLine();
-            Console.Write("Digite o valor do saque: ");
-            int valor = Convert.ToInt16(Console.ReadLine());
-            int n100 = valor / 100;
-            int resto = valor - (n100 * 100); 
-            int n50 = resto / 50;
-            resto = resto - (n50 * 50);
-            int n20 = resto / 20;
-            resto = resto - (n20 * 20);
-            int n10 = resto / 10;
-            resto = resto - (n10 * 10);
-            int n5 = resto / 5; 
-            resto = resto - (n5 * 5);
-            int n2 = resto / 2;
-            resto = resto - (n2 * 2);
-            int n1 = resto / 1;
-            resto = resto - (n1 * 1);
+            // Console.WriteLine();
+            // Console.WriteLine("Caixa Eletronico");
+            // Console.WriteLine();
+            // Console.Write("Digite o valor do saque: ");
+            // int valor = Convert.ToInt16(Console.ReadLine());
+            // int n100 = valor / 100;
+            // int resto = valor - (n100 * 100); 
+            // int n50 = resto / 50;
+            // resto = resto - (n50 * 50);
+            // int n20 = resto / 20;
+            // resto = resto - (n20 * 20);
+            // int n10 = resto / 10;
+            // resto = resto - (n10 * 10);
+            // int n5 = resto / 5; 
+            // resto = resto - (n5 * 5);
+            // int n2 = resto / 2;
+            // resto = resto - (n2 * 2);
+            // int n1 = resto / 1;
+            // resto = resto - (n1 * 1);
 
-            Console.WriteLine();
-            Console.WriteLine("Quantidade de notas:");
-            Console.WriteLine();
-            Console.WriteLine("Notas de 100R$:  " +n100);
-            Console.WriteLine();
-            Console.WriteLine("Notas de 50R$:  " +n50);
-            Console.WriteLine();
-            Console.WriteLine("Notas de 20R$:  " +n20);
-            Console.WriteLine();
-            Console.WriteLine("Notas de 10R$:  " +n10);
-            Console.WriteLine();
-            Console.WriteLine("Notas de 5R$:  " +n5);
-             Console.WriteLine();
-            Console.WriteLine("Notas de 2R$:  " +n2);
-            Console.WriteLine();
-            Console.WriteLine("Notas de 1R$:  " +n1);
-            Console.WriteLine();
-            Console.WriteLine("Roubamos todo o seu dinheiro kkkkkk");
+            // Console.WriteLine();
+            // Console.WriteLine("Quantidade de notas:");
+            // Console.WriteLine();
+            // Console.WriteLine("Notas de 100R$:  " +n100);
+            // Console.WriteLine();
+            // Console.WriteLine("Notas de 50R$:  " +n50);
+            // Console.WriteLine();
+            // Console.WriteLine("Notas de 20R$:  " +n20);
+            // Console.WriteLine();
+            // Console.WriteLine("Notas de 10R$:  " +n10);
+            // Console.WriteLine();
+            // Console.WriteLine("Notas de 5R$:  " +n5);
+            // Console.WriteLine();
+            // Console.WriteLine("Notas de 2R$:  " +n2);
+            // Console.WriteLine();
+            // Console.WriteLine("Notas de 1R$:  " +n1);
+            // Console.WriteLine();
+            // Console.WriteLine("Roubamos todo o seu dinheiro kkkkkk");
+
+            int[] notas = {1, 2, 5, 10, 20, 50, 100};
+
+            System.Console.Write("Digite o valor do seu saque: ");
+            int saque = int.Parse(Console.ReadLine());
+            DateTime data = DateTime.Now;
+
+            for (int i = notas.Length - 1; i >= 0 ; i--) {
+
+                int qntdNotas = saque / notas[i];
+
+                int restante = saque % notas[i];
+                saque = restante;
+
+                if(qntdNotas != 0) {
+                    System.Console.WriteLine($" Você recebeu {qntdNotas} notas(s) de {notas[i]} em {data}");
+                }
+            }
         }
     }
 }
